17ca1c72fb2506f81e0552ca198473fc7dbd489d
Edward Tremel <high.order.bits@gmail.com>
2024-07-02 17:17:37 -0400

    Fixed a mistake in the string-only chaincode
    The value parameter needs to be declared as a string, not a []byte, even
    though these are supposed to be equivalent in Go. The intermediate
    layers in HLF treat all parameters as strings and can only convert them
    to other types if there is a JSON serialization/deserialization defined
    for them. That was the whole point of trying this setup instead of the
    "blob" chaincode with []byte data.

--- string-chaincode/chaincode/smartcontract.go
+++ string-chaincode/chaincode/smartcontract.go
@@ -10,18 +10,18 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-func (s *SmartContract) PutString(ctx contractapi.TransactionContextInterface, key string, value []byte) error {
-	return ctx.GetStub().PutState(key, value)
+func (s *SmartContract) PutString(ctx contractapi.TransactionContextInterface, key string, value string) error {
+	return ctx.GetStub().PutState(key, []byte(value))
 }
 
-func (s *SmartContract) GetString(ctx contractapi.TransactionContextInterface, key string) (*string, error) {
+func (s *SmartContract) GetString(ctx contractapi.TransactionContextInterface, key string) (string, error) {
 	rawData, err := ctx.GetStub().GetState(key)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read from world state: %v", err)
+		return "", fmt.Errorf("failed to read from world state: %v", err)
 	}
 	if rawData == nil {
-		return nil, fmt.Errorf("object with key %s does not exist", key)
+		return "", fmt.Errorf("object with key %s does not exist", key)
 	}
 	dataString := string(rawData)
-	return &dataString, nil
+	return dataString, nil
 }
