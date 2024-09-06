1e827219ac0c17d550e933aa53150204e9685940
Edward Tremel <high.order.bits@gmail.com>
2024-09-06 15:06:45 -0400

    Added a test module for Hyperledger Caliper
    I'm figuring out how to use Hyperledger Caliper to run my performance
    test instead of my custom Go application, and these are the
    configuration files and JavaScript code needed to create a Caliper
    module.

---
+++ caliper-workspace/putString.js
@@ -0,0 +1,85 @@
+
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+function randomString(length) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	const charactersLength = characters.Length;
+	let result = '';
+	let counter = 0;
+	while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+/**
+ * Workload module for the benchmark round.
+ */
+class PutStringWorkload extends WorkloadModuleBase {
+    /**
+     * Initializes the workload module instance.
+     */
+    constructor() {
+        super();
+        this.txIndex = 0;
+		this.dataSize = 100;
+		this.numSampleData = 100;
+		this.sampleData = [];
+    }
+    /**
+     * Initialize the workload module with the given parameters.
+     * @param {number} workerIndex The 0-based index of the worker instantiating the workload module.
+     * @param {number} totalWorkers The total number of workers participating in the round.
+     * @param {number} roundIndex The 0-based index of the currently executing round.
+     * @param {Object} roundArguments The user-provided arguments for the round from the benchmark configuration file.
+     * @param {BlockchainInterface} sutAdapter The adapter of the underlying SUT.
+     * @param {Object} sutContext The custom context object provided by the SUT adapter.
+     * @async
+     */
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+
+		this.dataSize = this.roundArguments.dataSize;
+		// The numDistinctObjects argument is optional, so it may not exist in the roundArguments object
+		if(this.roundArguments.hasOwnProperty('numDistinctObjects')) {
+			this.numSampleData = this.roundArguments.numDistinctObjects;
+		}
+		for(let i = 0; i < this.numSampleData; i++) {
+			this.sampleData.push(randomString(this.dataSize));
+		}
+	}
+
+
+    /**
+     * Assemble TXs for the round.
+     * @return {Promise<TxStatus[]>}
+     */
+    async submitTransaction() {
+        this.txIndex++;
+        let key = 'Client' + this.workerIndex + '_KEY' + this.txIndex.toString();
+		let value = this.sampleData[Math.floor(Math.random() * this.sampleData.length)];
+
+        let args = {
+            contractId: 'plain_string',
+            contractVersion: 'v1',
+            contractFunction: 'PutString',
+            contractArguments: [key, value],
+            timeout: 30
+        };
+
+        await this.sutAdapter.sendRequests(args);
+    }
+}
+
+/**
+ * Create a new instance of the workload module.
+ * @return {WorkloadModuleInterface}
+ */
+function createWorkloadModule() {
+    return new PutStringWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
